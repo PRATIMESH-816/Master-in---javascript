98bde5243b2486a0fa77193458dfec990bde2c1f
PRATIMESH-816 <pareshprashant13@gmail.com>
2025-10-20 01:57:53 +0000

    variables

---
+++ 01_build/var.js
@@ -0,0 +1,16 @@
+const accountId = 42
+let accountEmail = "Pratimesh@gmail.com";
+var accountPassword = 12345
+accountCity = "karnataka" 
+
+// accountId = 2 invaild or not allowed
+
+accountEmail="henry@gmail.com"
+accountPassword=1212312123;
+accountCity="maharastra"
+let account;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,account]);
+
+/* prefer not to use var because of some issue
+and var is not a block scope it is */
